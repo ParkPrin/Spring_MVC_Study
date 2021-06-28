a7df5d287184ffaf00271395605a495f1de65b5a
박 종훈 <m05214@naver.com>
2021-06-28 13:30:47 +0900

    RequestHeaderServlet과 학습완료 후 정리, HTTP 요청 데이터 학습시작

---
+++ servlet/README.md
@@ -0,0 +1,14 @@
+
+## HTTP 요청 데이터 - 개요
+
+### GET 쿼리 파라미터
+
+### POST HTML Form
+
+메세지 바디에 쿼리 파라미터 형식으로 전달
+
+### HTTP message body
+
+- HTTP API에서 주로 사용, JSON, XML, TEXT
+- 주력은 JSON
+- POST, PUT, FATCH 등을 사용
--- servlet/src/main/java/com/example/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/com/example/servlet/basic/request/RequestHeaderServlet.java
@@ -2,6 +2,7 @@ package com.example.servlet.basic.request;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,59 @@ public class RequestHeaderServlet extends HttpServlet {
         // extract method 기능:  ctrl + t 누른후 extract method 선택
         printHeaders(request);
         printStartLine(request);
+
+        printHeaderUtils(request);
+
+        printEtc(request);
+    }
+
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]"); // 서버에 접속한 클라이언트 정보
+        System.out.println("request.getRemoteHost() = " +
+                request.getRemoteHost()); //
+        System.out.println("request.getRemoteAddr() = " +
+                request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " +
+                request.getRemotePort()); //
+        System.out.println();
+        System.out.println("[Local 정보]"); // 서버에 접속한 클
+        System.out.println("request.getLocalName() = " +
+                request.getLocalName()); //
+        System.out.println("request.getLocalAddr() = " +
+                request.getLocalAddr()); //
+        System.out.println("request.getLocalPort() = " +
+                request.getLocalPort()); //
+        System.out.println("--- 기타 조회 end ---");
+        System.out.println();
+    }
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " + request.getServerName());
+        System.out.println("request.getServerPort() = " + request.getServerPort());
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " +locale));
+        System.out.println("request.getLocale() = " + request.getLocale()); // 최상위 locale 정보를 반환한다.
+
+        System.out.println();
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }}
+        System.out.println();
+
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
     }
 
     private void printHeaders(HttpServletRequest request) {
@@ -35,9 +89,9 @@ public class RequestHeaderServlet extends HttpServlet {
         String scheme = request.getScheme();
         System.out.println("scheme = " + scheme);
         StringBuffer requestURL = request.getRequestURL();
-        System.out.println("requestURL = " + requestURL);
+        System.out.println("requestURL = " + requestURL); // 전체 주소
         String requestURI = request.getRequestURI();
-        System.out.println("requestURI = " + requestURI);
+        System.out.println("requestURI = " + requestURI); // 자원 주소
         String queryString = request.getQueryString();
         System.out.println("queryString = " + queryString);
         boolean secure = request.isSecure();
