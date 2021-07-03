65231b0749cddf368b06f661a7e58633e4bf6477
박 종훈 <m05214@naver.com>
2021-07-03 23:12:58 +0900

    Servlet을 이용한 회원 저장, list 조회의 결과를 html을 그려서 전달함

---
+++ servlet/src/main/java/com/example/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,51 @@
+package com.example.servlet.web.servlet;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberReponsitory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    /**
+     * new-form Servlet으로 부터 받은 데이터를 Save 처리를 하고 나서
+     * 저장한 결과를 getWriter를 이용하여 전달하는 Servelt
+     */
+
+    private MemberReponsitory memberReponsitory = MemberReponsitory.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberReponsitory.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" + "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "    <li>id="+member.getId()+"</li>\n" +
+                "    <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" + "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" + "</body>\n" +
+                "</html>");
+    }
+}
